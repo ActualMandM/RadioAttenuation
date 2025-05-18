3c6554d473e928d1d972841b978541c406986a11
Michael <15317421+ActualMandM@users.noreply.github.com>
2025-05-17 22:37:09 -0700

    wip

---
+++ RadioAttenuation/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace RadioAttenuation
+{
+    [BepInPlugin("com.MandM.RadioAttenuation", "RadioAttenuation", "1.0.0")]
+    [BepInProcess("Bomb Rush Cyberfunk.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("MandM.RadioAttenuation.Harmony");
+            harmony.PatchAll();
+        }
+    }
+}
